a04748cff56f94ed7113d221dbd3e0fe8cfffcb4
jsphLim <1806291589@qq.com>
2018-02-11 01:37:49 +0800

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,6 +1,5 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
   networks: {
     development: {
       host: "127.0.0.1",
